897fa1e61aa1c207fd34ec5083d01fdd544be94e
Tomasz Sack <tomek.sack@gmail.com>
2024-07-24 23:02:15 +0200

    basic changes of design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import BlogAsset from './assets/blogAsset.png'
 
 type Post = {
   userId:number
@@ -42,13 +43,11 @@ function App() {
   useEffect(()=>{
       fetch('https://jsonplaceholder.typicode.com/posts')
           .then(response => response.json())
-          .then(json => setPosts(json))
+          .then(json => setPosts(json.sort(() => 0.5 - Math.random())))
       fetch('https://jsonplaceholder.typicode.com/users')
           .then(response => response.json())
           .then(json => setAuthors(json))
   },[])
-
-  console.log(authors)
 
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value)
@@ -59,23 +58,36 @@ function App() {
     if (!authors) return
 
     const person = authors.filter((author) => author.name === value )
+
+    if (person.length===0) return
     
-    fetch(`https://jsonplaceholder.typicode.com/posts${value && `?userId=${person[0].id}`}`)
+    fetch(`https://jsonplaceholder.typicode.com/posts${value && person.length>0 && `?userId=${person[0].id}`}`)
     .then(response => response.json())
     .then(json => setPosts(json))
   }
 
+  if (!authors) return
+
   return (
-    <div>
+    <div className='flex flex-col items-center p-6 bg-[#fafafa] text-[#121212]'>
+      <h1 className='text-5xl'>TOMASZ SACK <span className='text-[#f9423b]'>BLOG</span></h1>
       <form onSubmit={handleSubmit}>
         <input value={value} onChange={handleChange}></input>
         <button type='submit'>Submit</button>
       </form>
-      <div className='bg-[#fafafa] text-black'>
+      <div className='flex flex-wrap justify-around'>
       {
-        posts.map((post:Post)=>(
-          <p key={post.id}>{post.title}</p>
-        ))
+        posts.map((post:Post)=>{
+          const person = authors?.filter((author) => author.id===post.userId)
+          return(
+          <div key={post.id} className='w-[350px] h-[400px] flex flex-col m-6 shadow-lg'>
+            <img src={BlogAsset} alt="" className='object-cover' />
+            <div className='p-4'>
+              <p><span className='text-[#f9423b]'>created by </span>{person[0].name}</p>
+              <h2 className='font-bold text-xl'>{post.title.charAt(0).toUpperCase()+post.title.slice(1)}</h2>
+          </div>
+          </div>
+        )})
       }
       </div>
     </div>
