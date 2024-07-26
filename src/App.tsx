11b9b638f6b57ee0931e4b62a7c1d7f4a8048923
Tomasz Sack <tomek.sack@gmail.com>
2024-07-26 22:06:33 +0200

    fixed randomization, add const color theme

--- src/App.tsx
+++ src/App.tsx
@@ -32,39 +32,40 @@ function App() {
     
     fetch(`https://jsonplaceholder.typicode.com/posts${value && person.length>0 && `?userId=${person[0].id}`}`)
     .then(response => response.json())
-    .then(json => setPosts(json.sort(() => 0.5 - Math.random())))
+    .then(json => setPosts(json.length>0 ? json.sort(() => 0.5 - Math.random()): json))
   }
 
   if (!authors || !posts) return <div>Ooops... something went wrong</div>
 
   return (
-    <div className='flex flex-col items-center p-6 text-[#121212]'>
+    <div className='flex flex-col items-center p-6'>
       <div className='max-w-[1440px]'>
-      <h1 className='text-5xl text-center '>TOMASZ SACK <span className='text-[#f9423b]'>BLOG</span></h1>
-      <form className='text-center py-10' onSubmit={handleSubmit}>
-        <input className='text-xl border-2 border-[#f9423b] rounded-xl px-2' value={value} onChange={handleChange}></input>
-        <button className='relative left-[-28px]' type='submit'><FontAwesomeIcon className='text-[#f9423b]' size='lg' icon={faMagnifyingGlass} />  </button>
-        
-      </form>
-      <div className='flex flex-wrap justify-around'>
-      {
-        Array.isArray(posts) ? posts.map((post:Post)=>{
-          const person = authors?.filter((author) => author.id===post.userId)
-          return(
-          <div key={post.id} className='w-[400px] h-[400px] flex flex-col m-6 shadow-lg'>
-            <img src={BlogAsset} alt="" className='object-cover' />
-            <div className='p-4'>
-              <p><span className='text-[#f9423b]'>created by </span>{person[0].name}</p>
-              <h2 className='font-bold text-lg line-clamp-1 my-2'>{post.title.charAt(0).toUpperCase()+post.title.slice(1)}</h2>
-              <div className='text-sm line-clamp-3'>
-               {post.body.charAt(0).toUpperCase()+post.body.slice(1)}
+        <h1 className='text-5xl text-center '>TOMASZ SACK <span className='text-main-pink'>BLOG</span></h1>
+        <form className='text-center py-10' onSubmit={handleSubmit}>
+          <input className='text-xl border-2 border-main-pink rounded-xl px-2' value={value} onChange={handleChange}></input>
+          <button className='relative left-[-28px]' type='submit'>
+            <FontAwesomeIcon className='text-main-pink' size='lg' icon={faMagnifyingGlass} />
+          </button>
+        </form>
+        <div className='flex flex-wrap justify-around'>
+        {
+          Array.isArray(posts) ? posts.map((post:Post)=>{
+            const person = authors?.filter((author) => author.id===post.userId)
+            return(
+            <div key={post.id} className='w-[400px] h-[400px] flex flex-col m-6 shadow-lg'>
+              <img src={BlogAsset} alt="" className='object-cover' />
+              <div className='p-4'>
+                <p><span className='text-main-pink'>created by </span>{person[0].name}</p>
+                <h2 className='font-bold text-lg line-clamp-1 my-2'>{post.title.charAt(0).toUpperCase()+post.title.slice(1)}</h2>
+                <div className='text-sm line-clamp-3'>
+                  {post.body.charAt(0).toUpperCase()+post.body.slice(1)}
+                </div>
+                <a className='pt-auto text-sm text-main-pink cursor-pointer'>Read more<FontAwesomeIcon className='pl-1' icon={faArrowRight} /></a>
               </div>
-              <a className='pt-auto text-sm text-[#f9423b] cursor-pointer'>Read more<FontAwesomeIcon className='pl-1' icon={faArrowRight} /></a>
-          </div>
-          </div>
-        )}) : <p>No available articles of this author. Try again.</p>
-      }
-      </div>
+            </div>
+          )}) : <p>No available articles of this author. Try again.</p>
+        }
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'main-pink': '#f9423b'
+      }
+    },
   },
   plugins: [],
 }
