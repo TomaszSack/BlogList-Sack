0bbfe99a3dfae937526ebc5add33871f23279af3
Tomasz Sack <tomek.sack@gmail.com>
2024-07-26 21:52:10 +0200

    fixed empty input searching

--- src/App.tsx
+++ src/App.tsx
@@ -29,18 +29,16 @@ function App() {
     if (!authors) return
 
     const person = authors.filter((author) => author.name === value )
-
-    if (person.length===0) return
     
     fetch(`https://jsonplaceholder.typicode.com/posts${value && person.length>0 && `?userId=${person[0].id}`}`)
     .then(response => response.json())
-    .then(json => setPosts(json))
+    .then(json => setPosts(json.sort(() => 0.5 - Math.random())))
   }
 
   if (!authors || !posts) return <div>Ooops... something went wrong</div>
 
   return (
-    <div className='flex flex-col items-center p-6 bg-[#fafafa] text-[#121212]'>
+    <div className='flex flex-col items-center p-6 text-[#121212]'>
       <div className='max-w-[1440px]'>
       <h1 className='text-5xl text-center '>TOMASZ SACK <span className='text-[#f9423b]'>BLOG</span></h1>
       <form className='text-center py-10' onSubmit={handleSubmit}>
@@ -50,7 +48,7 @@ function App() {
       </form>
       <div className='flex flex-wrap justify-around'>
       {
-        posts.map((post:Post)=>{
+        Array.isArray(posts) ? posts.map((post:Post)=>{
           const person = authors?.filter((author) => author.id===post.userId)
           return(
           <div key={post.id} className='w-[400px] h-[400px] flex flex-col m-6 shadow-lg'>
@@ -61,10 +59,10 @@ function App() {
               <div className='text-sm line-clamp-3'>
                {post.body.charAt(0).toUpperCase()+post.body.slice(1)}
               </div>
-              <a className='text-sm text-[#f9423b]'>Read more<FontAwesomeIcon className='pl-1' icon={faArrowRight} /></a>
+              <a className='pt-auto text-sm text-[#f9423b] cursor-pointer'>Read more<FontAwesomeIcon className='pl-1' icon={faArrowRight} /></a>
           </div>
           </div>
-        )})
+        )}) : <p>No available articles of this author. Try again.</p>
       }
       </div>
       </div>
